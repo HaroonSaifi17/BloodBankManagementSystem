a73261666278a420f61a6e9ddb6fa5370256382c
Mohd Haroon <haroonsaifi2001@gmail.com>
2023-04-16 06:12:41 +0530

    adding navbar

---
+++ public/script.js
@@ -0,0 +1,15 @@
+function out(){
+var n2 = document.getElementById("n2")
+var n3 = document.getElementById("n3")
+var n4 = document.getElementById("n4")
+  if(n2.style.right=='0px'){
+    n2.style.right='-50vw'
+    n4.style.display='none'
+    n3.style.display='inline-block'
+  }
+  else{
+    n2.style.right='0'
+    n3.style.display='none'
+    n4.style.display='inline-block'
+  }
+}
