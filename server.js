ad7798fa63912bd3df88d3541a15ace20232e259
Mohd Haroon <haroonsaifi2001@gmail.com>
2023-04-18 07:01:01 +0530

    some finish work

--- server.js
+++ server.js
@@ -46,23 +46,51 @@ app.get('/donorform', (req, res) => {
     res.status(500).json(error.message)
   }
 })
+app.get('/patientform', (req, res) => {
+  try {
+    res.render('blood/patientform')
+  } catch (error) {
+    res.status(500).json(error.message)
+  }
+})
 app.post('/donorform', (req, res) => {
   try {
-
-    connection.query(`INSERT INTO donors  (name, age, gender,blood_group, phone_number, email,address) VALUES ( '${req.body.name}', ${req.body.age}, '${req.body.gender}', '${req.body.blood_group}', '${req.body.phone_number}', '${req.body.email}','dumy address')`, (error, results1, fields) => {
-      if (error) {
-        console.error('Error querying database:', error)
+    connection.query(
+      `INSERT INTO donors  (name, age, gender,blood_group, phone_number, email,address) VALUES ( '${req.body.name}', ${req.body.age}, '${req.body.gender}', '${req.body.blood_group}', '${req.body.phone_number}', '${req.body.email}','dumy address')`,
+      (error, results1, fields) => {
+        if (error) {
+          console.error('Error querying database:', error)
+        }
+        res.render('blood/index')
       }
-    res.render('blood/index')
-    })
+    )
+  } catch (error1) {
+    res.status(500).json(error1.message)
+  }
+})
+app.post('/patientform', (req, res) => {
+  try {
+    connection.query(
+      `INSERT INTO recipients  (name, age, gender,blood_group, phone_number, email,address) VALUES ( '${req.body.name}', ${req.body.age}, '${req.body.gender}', '${req.body.blood_group}', '${req.body.phone_number}', '${req.body.email}','dumy address')`,
+      (error, results1, fields) => {
+        if (error) {
+          console.error('Error querying database:', error)
+        }
+        res.render('blood/index')
+      }
+    )
   } catch (error1) {
     res.status(500).json(error1.message)
   }
 })
 app.get('/adminlogin', (req, res) => {
   try {
-    errorMessage = ''
-    res.render('blood/adminlogin', { errorMessage })
+    if (!req.session.isAuthenticated) {
+      errorMessage = ''
+      res.render('blood/adminlogin', { errorMessage })
+    } else {
+      res.redirect('admindashboard')
+    }
   } catch (error) {
     res.status(500).json(error.message)
   }
@@ -80,14 +108,66 @@ app.get('/admindashboard', (req, res) => {
   if (!req.session.isAuthenticated) {
     res.redirect('/adminlogin')
   } else {
-    connection.query('SELECT blood_group,count(*) from blood_donations group by blood_group', (error, results1, fields) => {
-      if (error) {
-        console.error('Error querying database:', error)
-      } else {
-        let results = results1
-    res.render('blood/admindashboard',{results})
+    connection.query(
+      'SELECT blood_group,count(*) from blood_donations group by blood_group',
+      (error, results1, fields) => {
+        if (error) {
+          console.error('Error querying database:', error)
+        } else {
+          let results = results1
+          let obj = { blood_group: 'A+', 'count(*)': 0 }
+          if (
+            !results.some((result) => result.blood_group === obj.blood_group)
+          ) {
+            results.push(obj)
+          }
+          obj = { blood_group: 'O+', 'count(*)': 0 }
+          if (
+            !results.some((result) => result.blood_group === obj.blood_group)
+          ) {
+            results.push(obj)
+          }
+          obj = { blood_group: 'B+', 'count(*)': 0 }
+          if (
+            !results.some((result) => result.blood_group === obj.blood_group)
+          ) {
+            results.push(obj)
+          }
+          obj = { blood_group: 'AB+', 'count(*)': 0 }
+          if (
+            !results.some((result) => result.blood_group === obj.blood_group)
+          ) {
+            results.push(obj)
+          }
+          obj = { blood_group: 'A-', 'count(*)': 0 }
+          if (
+            !results.some((result) => result.blood_group === obj.blood_group)
+          ) {
+            results.push(obj)
+          }
+          obj = { blood_group: 'O+', 'count(*)': 0 }
+          if (
+            !results.some((result) => result.blood_group === obj.blood_group)
+          ) {
+            results.push(obj)
+          }
+          obj = { blood_group: 'B-', 'count(*)': 0 }
+          if (
+            !results.some((result) => result.blood_group === obj.blood_group)
+          ) {
+            results.push(obj)
+          }
+          obj = { blood_group: 'AB-', 'count(*)': 0 }
+          if (
+            !results.some((result) => result.blood_group === obj.blood_group)
+          ) {
+            results.push(obj)
+          }
+
+          res.render('blood/admindashboard', { results })
+        }
       }
-    })
+    )
   }
 })
 app.get('/admindonordetail', (req, res) => {
@@ -99,7 +179,7 @@ app.get('/admindonordetail', (req, res) => {
         console.error('Error querying database:', error)
       } else {
         let results = results1
-    res.render('blood/admindonordetail',{results})
+        res.render('blood/admindonordetail', { results })
       }
     })
   }
@@ -113,15 +193,14 @@ app.get('/adminpatientdetail', (req, res) => {
         console.error('Error querying database:', error)
       } else {
         let results = results1
-    res.render('blood/adminpatientdetail',{results})
+        res.render('blood/adminpatientdetail', { results })
       }
     })
   }
 })
 app.get('/logout', (req, res) => {
-    req.session.isAuthenticated = false
-    res.redirect('/')
-
+  req.session.isAuthenticated = false
+  res.redirect('/')
 })
 
 app.listen(process.env.PORT || port, () => {
